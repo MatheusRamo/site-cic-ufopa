45585aee9d0946a2d47d17dff6bcc0d5aad2ad12
Matheus <geof.matheus@gmail.com>
2020-12-19 17:03:19 -0300

    modificado os cards

--- components/Menu/index.js
+++ components/Menu/index.js
@@ -13,7 +13,7 @@ export default function Menu({ fixed }) {
               <div className="w-full relative flex justify-between lg:w-auto px-4 lg:static lg:block lg:justify-start">
                 <a
                   className="text-sm font-bold text-center leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
-                  href="#pablo"
+                  href="#"
                 >
                   Números
                 </a>
--- pages/index.js
+++ pages/index.js
@@ -20,21 +20,21 @@ const Index = () => {
             <div className="text-center grid grid-cols-1  p-2 bg-indigo-100 sm:grid-cols-2 sm:mx-20 md:grid-cols-3 md:mx-10 lg:grid-cols-4 lg:mx-35 xl:mx-50 xl:grid-cols-5">
 
 
-                <Card titulo="Números" descricao="Número é um objeto abstrato da matemática usado pa." imagem="/integral.png" color="bg-red-300" hoverColor="bg-red-200" link="/posts/numeros" />
+                <Card titulo="Números" descricao="Númeremática usado pa." imagem="/integral.png" color="bg-red-300" hoverColor="bg-red-200" link="/posts/numeros" />
 
-                <Card titulo="Teste" descricao="Número é um objeto abstrato da matemática usado pa." imagem="/integral.png" color="bg-blue-300" hoverColor="bg-blue-200" link="/posts/numeros" />
+                <Card titulo="Teste" descricao="Número é um mática usado pa." imagem="/integral.png" color="bg-blue-300" hoverColor="bg-blue-200" link="/posts/numeros" />
 
-                <Card titulo="Adição" descricao="Número é um objeto abstrato da matemática usado." imagem="/integral.png" color="bg-red-200" hoverColor="bg-red-100" link="/posts/numeros" />
+                <Card titulo="Adição" descricao="Número é um objeto abstrato da" imagem="/integral.png" color="bg-red-200" hoverColor="bg-red-100" link="/posts/numeros" />
 
                 <Card titulo="Numeros" descricao="Número é um objeto abstrato." imagem="/integral.png" color="bg-green-400" hoverColor="bg-green-300" link="/posts/numeros" />
 
-                <Card titulo="Teste" descricao="Número é um objeto abstrato da matemática usado pa." imagem="/integral.png" color="bg-blue-300" hoverColor="bg-blue-200" link="/posts/numeros" />
+                <Card titulo="Teste" descricao="Número é um objeto abstrato da " imagem="/integral.png" color="bg-blue-300" hoverColor="bg-blue-200" link="/posts/numeros" />
 
-                <Card titulo="Teste" descricao="Número é um objeto abstrato da matemática usado pa." imagem="/integral.png" color="bg-blue-300" hoverColor="bg-blue-200" link="/posts/numeros" />
+                <Card titulo="Teste" descricao="Número é um objeto abstrato da ." imagem="/integral.png" color="bg-blue-300" hoverColor="bg-blue-200" link="/posts/numeros" />
 
-                <Card titulo="Teste" descricao="Número é um objeto abstrato da matemática usado pa." imagem="/derivada.png" color="bg-blue-200" hoverColor="bg-blue-100" link="/posts/numeros" />
+                <Card titulo="Teste" descricao="Número é um objeto abstrato pa." imagem="/derivada.png" color="bg-blue-200" hoverColor="bg-blue-100" link="/posts/numeros" />
 
-                <Card titulo="Teste" descricao="Número é um objeto abstrato da matemática usado pa." imagem="/derivada.png" color="bg-yellow-300" hoverColor="bg-yellow-200" link="/posts/numeros" />
+                <Card titulo="Teste" descricao="Número é um objeto abstrato a." imagem="/derivada.png" color="bg-yellow-300" hoverColor="bg-yellow-200" link="/posts/numeros" />
 
             </div>
         </div>
